027ea0e537e341e9291caa7b8497a5c18437664e
[hyunba] <dls272@naver.com>
2022-01-12 19:51:17 +0900

    category

--- components/ToDo.tsx
+++ components/ToDo.tsx
@@ -1,9 +1,15 @@
 import { IToDo } from "./atoms";
 
-function ToDo({text}:IToDo) {
+function ToDo({text, category}:IToDo) {
+    const onClick = (event:React.MouseEvent<HTMLButtonElement>) => {
+        console.log("I wnane go to", event.currentTarget.name);
+    };
     return (
         <li>
-            {text}
+            <span>{text}</span>
+            {category !== "DOING" && <button name="DOING" onClick={onClick}>Doing</button>}
+            {category !== "TO_DO" && <button name="TO_DO" onClick={onClick}>To Do</button>}
+            {category !== "DONE" && <button name="DONE" onClick={onClick}>Done</button>}
         </li>);
 }
 
